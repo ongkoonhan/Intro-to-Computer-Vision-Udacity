90f67fbe1a9fa9b6f1670a69f85987aa6a0cb5b3
Ong <koonhanong@kpmg.com.sg>
2018-05-17 16:44:46 +0800

    continued problem set

--- scripts/problem_sets/ps0_python/ps0.py
+++ scripts/problem_sets/ps0_python/ps0.py
@@ -28,24 +28,24 @@ img_1_swap = img_1[:,:,swap_index]
 
 utils.print_image_details_and_show(img_1_swap, "after swap")
 
-img_1_swap_path = os.path.join(output_folder, "ps0-2-a-1.png")
-cv.imwrite(img_1_swap_path, img_1_swap)
+img_path = os.path.join(output_folder, "ps0-2-a-1.png")
+cv.imwrite(img_path, img_1_swap)
 
 
 # (b)
 img_1_green = img_1[:,:,1]   # BGR
 utils.print_image_details_and_show(img_1_green, "green")
 
-img_1_green_path = os.path.join(output_folder, "ps0-2-b-1.png")
-cv.imwrite(img_1_green_path, img_1_green)
+img_path = os.path.join(output_folder, "ps0-2-b-1.png")
+cv.imwrite(img_path, img_1_green)
 
 
 # (c)
 img_1_red = img_1[:,:,2]   # BGR
 utils.print_image_details_and_show(img_1_red, "red")
 
-img_1_red_path = os.path.join(output_folder, "ps0-2-c-1.png")
-cv.imwrite(img_1_red_path, img_1_red)
+img_path = os.path.join(output_folder, "ps0-2-c-1.png")
+cv.imwrite(img_path, img_1_red)
 
 
 ### Q3
@@ -64,8 +64,50 @@ img_1_red_center[rows_red_l:rows_red_u, cols_red_l:cols_red_u] = \
 
 utils.print_image_details_and_show(img_1_red_center, "center")
 
-img_1_red_center_path = os.path.join(output_folder, "ps0-3-a-1.png")
-cv.imwrite(img_1_red_center_path, img_1_red_center)
+img_path = os.path.join(output_folder, "ps0-3-a-1.png")
+cv.imwrite(img_path, img_1_red_center)
+
+
+### Q4
+# (a)
+min_pix_val, max_pix_val, minLoc, maxLoc	= cv.minMaxLoc(img_1_green)
+mean_pix_val, sd_pix_val = cv.meanStdDev(img_1_green)
+# max_pix_val = np.uint8(np.amax(img_1_green))
+# min_pix_val = np.uint8(np.amin(img_1_green))
+# mean_pix_val = np.uint8(np.mean(img_1_green))
+# sd_pix_val = np.uint8(np.std(img_1_green))
+
+print("max: {0}, min: {1}, mean: {2}, SD: {3}" \
+      .format(max_pix_val, min_pix_val, mean_pix_val, sd_pix_val)
+)
+
+
+# (b)
+img_1_green_normalized = img_1_green.copy()
+img_1_green_normalized = cv.subtract(img_1_green_normalized, mean_pix_val)
+img_1_green_normalized = cv.divide(img_1_green_normalized, sd_pix_val)
+img_1_green_normalized = cv.multiply(img_1_green_normalized, 10)
+img_1_green_normalized = cv.add(img_1_green_normalized, mean_pix_val)
+
+utils.print_image_details_and_show(img_1_green_normalized, "green_normalized")
+
+img_path = os.path.join(output_folder, "ps0-4-b-1.png")
+cv.imwrite(img_path, img_1_green_normalized)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
